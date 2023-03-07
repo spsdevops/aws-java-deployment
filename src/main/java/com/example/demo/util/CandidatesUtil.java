1d007a273504024be7b81e57836e77bf53440d16
sjaviles <88112990+sjaviles@users.noreply.github.com>
2023-03-07 09:53:21 -0600

    Sube proyecto de prueba java gradle

---
+++ src/main/java/com/example/demo/util/CandidatesUtil.java
@@ -0,0 +1,28 @@
+package com.example.demo.util;
+
+import java.util.List;
+import java.util.Set;
+
+import com.example.demo.model.Candidate;
+
+public final class CandidatesUtil {
+    private CandidatesUtil() {
+	}
+
+    /**
+	 * 
+	 * Method to help us get dummy data
+	 * 
+	 * @return list of candidates
+	 */
+	public static List<Candidate> getCandidates() {
+		var candidateList = List.of(
+				new Candidate("Sam 1", 2, Set.of("java", "c#", "c++", "golang")),
+				new Candidate("Sam 2", 1, Set.of("javascript", "c++")),
+				new Candidate("Sam 3", 4, Set.of("java", "python")),
+				new Candidate("Sam 4", 2, Set.of("ruby", "aws", "docker")),
+				new Candidate("Sam 5", 3, Set.of("python", "Erlang", "docker")));
+
+		return candidateList;
+	}
+}
